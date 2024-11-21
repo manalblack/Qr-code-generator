ede90d61d70db1549d1f1bd557f9d6223544c950
manal-coder <134100535+manal-coder@users.noreply.github.com>
2024-11-20 21:42:03 -0800

    Initial commit

---
+++ code.js
@@ -0,0 +1,15 @@
+
+
+const generate = document.querySelector('.generate');
+const qrImage = document.getElementById('qrImg')
+const input = document.querySelector('.input');
+
+function getApi() {
+    
+    generate.addEventListener('click', () => {
+        qrImage.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${input.value}`;
+        input.value = '';
+    })
+}
+
+getApi();
